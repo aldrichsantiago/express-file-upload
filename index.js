d38a973fb26d7aa6e153e77f2f24ad4de694d423
Aldrich Santiago <aldrichsantiago20@gmail.com>
2023-12-20 20:23:52 +0800

    added image compression and conversion to webp

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import multer from "multer";
 import path from "path"
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
+import fs from "fs"
+import sharp from "sharp";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,27 +12,53 @@ const PORT = 8000;
 const SERVER_URL = `http://localhost:${PORT}/`;
 
 // Set up Multer storage
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/'); // Set the destination folder for uploaded files
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Set the file name to be unique
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, 'public/'); // Set the destination folder for uploaded files
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, Date.now() + '-' + file.originalname); // Set the file name to be unique
+//     }
+// });
+
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage: storage });
 
 const app = express();
 
+app.use(express.static('public'))
 
 app.get(`/`, (req, res)=>{
-    res.sendFile(path.join(__dirname, '/index.html'))
+  res.send("HELLO API WORKING")
 })
 
-app.post(`/single-image`,upload.single('image'), (req, res)=>{
+app.get(`/single-image`,upload.single('image'), (req, res)=>{
 
-    res.send('File uploaded successfully!');
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+
+
+
+app.post(`/single-image`, upload.single('image'), async(req, res)=>{
+
+  console.log(req.file)
+  fs.access("./public", (error) => {
+    if (error) {
+      fs.mkdirSync("./public");
+    }
+  });
+  const { buffer, originalname } = req.file;
+  const timestamp = Date.now()
+  const ref = `${timestamp}-${originalname.split('.')[0]}.webp`;
+  await sharp(buffer)
+    .webp({ quality: 20 })
+    .toFile(`./public/${ref}`)
+    .then(info => { console.log("Info: " + JSON.stringify(info, null, 2)) })
+    .catch(err => { console.log("Error: " + err) });
+  const link = `${SERVER_URL}${ref}`;
+  return res.json({ link });
 })
 
 
